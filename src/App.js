76ceb94f1e1b9919cbbb636f4010710aa2f14067
Xavi <xavings@gmail.com>
2017-09-06 16:22:47 +0200

    Monkey patching other currencies. Using percentage instead

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 import { LineChart, Line ,CartesianGrid, XAxis, YAxis, Tooltip} from 'recharts';
 import Axios from 'axios'
 
-const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
-
-
-class App extends Component {
-
-
+class App extends Component
+{
     constructor (props)
     {
         super()
@@ -35,25 +23,123 @@ class App extends Component {
                 oneDay:86400
             }
 
-        //let baseUrl= 'https://poloniex.com/public?command=returnChartData&currencyPair=BTC_XMR&end=9999999999&period=14400&start=1405699200'
-        
-        Axios.get('https://poloniex.com/public',{
-                    params:{
-                      command: 'returnChartData',
-                      currencyPair: 'BTC_XMR',
-                      period:period.oneDay,
-                      end:endTimestamp,
-                      start:startTimestamp
-                    }
-                })
-            .then((result)=> {
-                this.setState({data:result.data})
-                console.log(result)
-          });
-
-
         this.state = {data:[]}
+        //let baseUrl= 'https://poloniex.com/public?command=returnChartData&currencyPair=BTC_XMR&end=9999999999&period=14400&start=1405699200'
+
+        let main = []
+        Axios.get('https://poloniex.com/public',{
+                params:{
+                command: 'returnChartData',
+                currencyPair: 'BTC_XMR',
+                period:period.oneDay,
+                end:endTimestamp,
+                start:startTimestamp
+                }
+            })
+        .then((result)=> {
+
+            let referenceValue = result.data[0].weightedAverage
+
+            result.data.map(function(x, index)
+            {
+                if(!main[index])
+                    main[index] = {}
+
+                main[index]['BTC_XMR'] = x.weightedAverage/referenceValue
+                
+            })
+
+            this.setState({data:main})
+            console.log(main)
+        });
+
+
+        Axios.get('https://poloniex.com/public',{
+                params:{
+                command: 'returnChartData',
+                currencyPair: 'BTC_ETH',
+                period:period.oneDay,
+                end:endTimestamp,
+                start:startTimestamp
+            }
+        })
+        .then((result)=> {
+
+            let referenceValue = result.data[0].weightedAverage
+
+            result.data.map(function(x, index)
+            {
+                if(!main[index])
+                    main[index] = {}
+
+                main[index]['BTC_ETH'] = x.weightedAverage/referenceValue
+                
+            })
+
+            this.setState({Data:main})
+            console.log(main)
+        });
+
+
+        Axios.get('https://poloniex.com/public',{
+                params:{
+                command: 'returnChartData',
+                currencyPair: 'USDT_BTC',
+                period:period.oneDay,
+                end:endTimestamp,
+                start:startTimestamp
+                }
+        })
+        .then((result)=> {
+            console.log(result)
+            let referenceValue = result.data[0].weightedAverage
+
+            result.data.map(function(x, index)
+            {
+                if(!main[index])
+                    main[index] = {}
+
+                main[index]['BTC_USDT'] = 1/(x.weightedAverage/referenceValue)
+                
+            })
+
+            this.setState({Data:main})
+            console.log(main)
+        });
+
+        Axios.get('https://poloniex.com/public',{
+                params:{
+                command: 'returnChartData',
+                currencyPair: 'BTC_LTC',
+                period:period.oneDay,
+                end:endTimestamp,
+                start:startTimestamp
+                }
+        })
+        .then((result)=> {
+            console.log(result)
+            let referenceValue = result.data[0].weightedAverage
+
+            result.data.map(function(x, index)
+            {
+                if(!main[index])
+                    main[index] = {}
+
+                main[index]['BTC_LTC'] = 1/(x.weightedAverage/referenceValue)
+                
+            })
+
+            this.setState({Data:main})
+            console.log(main)
+        });
+
+    
+
+
+        
     }
+
+
 
     render() {
 
@@ -63,14 +149,18 @@ class App extends Component {
                 <unga/>
                     <LineChart width={900} height={500} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                        
-                        <Line type="monotone" dataKey="high" stroke="#88ffd8" dot = {false}/>
-                        <Line type="monotone" dataKey="low" stroke="#ff84d8" dot = {false} />
+                        <Line type="monotone" dataKey="BTC_ETH" stroke="#88ffd8" dot = {false}/>
+                        <Line type="monotone" dataKey="BTC_XMR" stroke="#ff84d8" dot = {false} />
+                        <Line type="monotone" dataKey="BTC_USDT" stroke="#888488" dot = {false} />
+                        <Line type="monotone" dataKey="BTC_LTC" stroke="#833488" dot = {false} />
                         
                         
                         <XAxis dataKey="name" />
                         <YAxis />
                         <Tooltip />
                     </LineChart>
+
+
             </div>
         );
     }
