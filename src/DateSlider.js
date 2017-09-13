3eb51ecc06bc6ade807dd0f133d4cd54aa5975f6
Xavi <xavings@gmail.com>
2017-09-13 15:42:24 +0200

    Got fthe friking slider behaving as intended...

--- src/DateSlider.js
+++ src/DateSlider.js
@@ -17,7 +17,7 @@ class DateSlider extends React.Component {
             startDate: 40,
             endDate:60,
             reference:50,
-            referencePosition:1,
+            referenceIndex:1,
             beforeReferenceIndex:1,
             beforeValues:[],
             values:[40,50,60]
@@ -45,24 +45,38 @@ class DateSlider extends React.Component {
         for(let i=0; i<exists.length; i++)
             if(!exists[i])
                 return i
+            
 
+        for(let i=0; i<beforeValues.length; i++)
+            if(beforeValues[i] != currenValues[i])
+                return i
+
+        console.log("AAA")
         return -1
     }
 
-    onReferenceChanged=(value, position)=>
+    onReferenceChanged=(value, currentIndex)=>
     {
         this.setState({
             'reference':value,
-            'referencePosition':position
+            'referenceIndex':currentIndex
         })
     }
 
-    onStartDateChanged=(value, position)=>
+    onStartDateChanged=(value, currentIndex)=>
     {
-        this.setState({'startDate':value})
+        let referenceIndex = this.state.referenceIndex
+        if(currentIndex ==  this.state.referenceIndex)
+
+
+
+        this.setState({
+            'startDate':value,
+            'referenceIndex':currentIndex
+        })
     }
 
-    onEndDateChanged=(value, position)=>
+    onEndDateChanged=(value, currentIndex)=>
     {
         this.setState({'endDate':value})
     }
@@ -72,15 +86,75 @@ class DateSlider extends React.Component {
     onChange = (values) => {
  
         let changedIndex = this.getChangedIndex(this.state.beforeValues, values)
+        let changingIndex = this.getChangedIndex(this.state.values, values)
 
+
+        let startDate = this.state.startDate
+        let endDate = this.state.endDate
+        let reference = this.state.reference
+        let referenceIndex = this.state.referenceIndex
+
+
+
+        console.log("in",changedIndex, changingIndex)
+        
+        if(changedIndex == -1)
+            return
+
+        for(let i=0; i<values.length; i++)
+        {
+            if(changedIndex==values[i])
+                changingIndex = i
+        }
+
+        //reference is changing
         if(changedIndex == this.state.beforeReferenceIndex)
-            this.onReferenceChanged(values[changedIndex], changedIndex)
-        else if(changedIndex<2)
-            this.onStartDateChanged(values[changedIndex], changedIndex)
-        else
-            this.onEndDateChanged(values[changedIndex], changedIndex)
+        {
+            console.log("ref")
+            reference = values[changedIndex]
+            referenceIndex = changingIndex
+        }
 
-        this.setState({values:values})
+        //start date is changing
+        else if(changedIndex<2)
+        {
+            console.log("start")
+            startDate = values[changedIndex]
+
+            if(this.state.referenceIndex == changingIndex)
+            {
+                if(changingIndex == 0)
+                    referenceIndex = 1
+                else  if(changingIndex == 1)
+                    referenceIndex = 0
+            }
+
+        }
+        //end date is changing
+        else
+        {
+            console.log("end")
+            endDate = values[changedIndex]
+            if(this.state.referenceIndex == changingIndex)
+            {
+                if(changingIndex == 1)
+                    referenceIndex = 2
+                
+                else if(changingIndex == 2)
+                    referenceIndex = 1
+
+            }
+        }
+
+
+
+        this.setState({
+            values:values,
+            startDate: startDate,
+            endDate:endDate,
+            reference:reference,
+            referenceIndex: referenceIndex
+        })
     }
 
     onBeforeChange=(values)=>
@@ -88,23 +162,33 @@ class DateSlider extends React.Component {
         //console.log('before',values)
         
         this.setState({
+            beforeReferenceIndex:this.state.referenceIndex,
             beforeValues:values
         })
     } 
 
     getValues=()=>
     {
-        console.log(this.state.startDate,this.state.reference, this.state.endDate)
+        //console.log(this.state.startDate,this.state.reference, this.state.endDate)
         //let values = [this.state.startDate,this.state.endDate]
-        //values.splice(this.state.referencePosition, 0, this.state.reference)    
+        //values.splice(this.state.referenceIndex, 0, this.state.reference)    
         //console.log('render',values)
         return [this.state.startDate, this.state.reference, this.state.endDate]
+    }
+
+    getHandleStyles=()=>
+    {
+        let baseColor = 'blue'
+        let referenceColor = 'yellow'
+        let handles = [{ backgroundColor: baseColor }, { backgroundColor: baseColor }, { backgroundColor: baseColor }]
+        handles[this.state.referenceIndex].backgroundColor = referenceColor
+        return handles
     }
 
     render() {
         let values = this.getValues()
 
-        console.log(values)
+       //console.log(values)
         
         return (
             <div style={style}>
@@ -113,7 +197,7 @@ class DateSlider extends React.Component {
                     onChange={this.onChange}
                     onBeforeChange={this.onBeforeChange}
                     trackStyle={[{ backgroundColor: 'red' }, { backgroundColor: 'green' }]}
-                    handleStyle={[{ backgroundColor: 'yellow' }, { backgroundColor: 'gray' }]}
+                    handleStyle={this.getHandleStyles()}
                     railStyle={{ backgroundColor: 'grey' }}
                     />
             </div>
